7640432cf6df41ea91926d774420618308d88115
Ciro Moniz <ciro@brlink.org>
2022-12-25 12:57:24 -0300

    Tratamento de erro em falha no SSH - issue #2
    Tratamento de erro em caso de falha ao realizar o script

--- bckp-mikrotik-email-telegram.sh
+++ bckp-mikrotik-email-telegram.sh
@@ -10,7 +10,16 @@ MK_BCKP_FILE=backup.rsc
 
 # Conectar ao Mikrotik via SSH e baixar o arquivo de backup
 sshpass -p $MK_PASS ssh $MK_USER@$MK_IP "export file=$MK_BCKP_FILE"
+if [ $? -ne 0 ]; then
+  echo "Erro ao se conectar ao Mikrotik via SSH"
+  exit 1
+fi
+
 sshpass -p $MK_PASS scp $MK_USER@$MK_IP:/$MK_BCKP_FILE .
+if [ $? -ne 0 ]; then
+  echo "Erro ao fazer download do arquivo de backup"
+  exit 1
+fi
 
 # Exibir uma mensagem de sucesso
 echo "Arquivo de backup baixado com sucesso!"
