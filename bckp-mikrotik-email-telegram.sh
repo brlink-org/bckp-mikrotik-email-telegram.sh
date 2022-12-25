912533d601b89ba294a1800b08598ec23a902a34
Ciro Moniz <ciro@brlink.org>
2022-12-25 16:32:29 -0300

    Remove verificação de se o arquivo existe issue #2
    Um passo atrás, remove a verificação se o existe no mikrotik, para tornar o script novamente funcional.

--- bckp-mikrotik-email-telegram.sh
+++ bckp-mikrotik-email-telegram.sh
@@ -10,32 +10,25 @@ MK_BCKP_FILE=backup.rsc
 
 
 
-# Tentar se conectar ao Mikrotik via SSH
+# Tenta se conectar ao Mikrotik via SSH
 sshpass -p $MK_PASS ssh $MK_USER@$MK_IP "echo conectado via SSH no Mikrotik"
 if [ $? -ne 0 ]; then
   echo "Erro: não foi possível se conectar ao Mikrotik via SSH"
   exit 1
 fi
 
-# Verificar se o arquivo existe no Mikrotik
-IFILE_MK=$(sshpass -p $MK_PASS ssh $MK_USER@$MK_IP "file print where name=$MK_BCKP_FILE")
-if ! grep -q "^$MK_BCKP_FILE" <<< "$IFILE_MK"; then
-  echo "Erro: arquivo não encontrado no Mikrotik"
-  exit 1
-fi
-
-# Fazer o download do arquivo
+# Faz o download do arquivo
 sshpass -p $MK_PASS scp $MK_USER@$MK_IP:/$MK_BCKP_FILE .
 if [ $? -ne 0 ]; then
   echo "Erro ao fazer download do arquivo de backup"
   exit 1
 fi
 
-# Verificar se o arquivo de backup foi realmente baixado
+# Verifica se o arquivo de backup foi realmente baixado
 if [ ! -f $MK_BCKP_FILE ]; then
   echo "Erro: arquivo de backup não foi baixado"
   exit 1
 fi
 
-# Exibir uma mensagem de sucesso
+# Exibi uma mensagem de sucesso
 echo "Arquivo de backup baixado com sucesso!"
