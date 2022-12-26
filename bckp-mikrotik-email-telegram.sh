8bacdacf5f9e6bc4e397071dfea0e69ef640e913
Ciro Moniz <ciro@brlink.org>
2022-12-26 19:30:01 -0300

    Acrescenta data no e-mail e msg telegram - Resolve #4
    Melhora os detalhes das mensagens de envio para o e-mail e telegram, que passam a ser a mesma capturando a data de envio;
    
    Remove as variáveis EMAIL_SUBJECT e EMAIL_BODY que não serão mais usadas substituídas pela variável MSG que captura a data;
    
    Modifica vairável EMAIL_TO para apenas EMAIL;
    
    Pequenos ajustes nos comentários para melhor explicar o script.

--- bckp-mikrotik-email-telegram.sh
+++ bckp-mikrotik-email-telegram.sh
@@ -8,48 +8,61 @@ MK_PASS=password
 MK_IP=172.16.0.1
 MK_BCKP_FILE=backup.rsc
 
-# Defina o opções de endereço
-EMAIL_TO=seu-email@gmail.com
-EMAIL_SUBJECT="Backup Mikrotik"
-EMAIL_BODY="Corpo do e-mail"
+# Defina o endereço de email
+EMAIL=seu-email@gmail.com
 
-# Define opções para o Telegram
+# Defina opções para o Telegram token e id do bot
 TOKEN="000000000:0000000000000-0000000000000000000000000000000"
 CHATID="-1234567890123"
 
-# Formato de compactação desejado: zip ou tar
+# Defina o formato de compactação desejado: "zip" ou "tar"
 COMPAC="zip"
 
 
 
-# Tenta se conectar ao Mikrotik via SSH
+# Não alterar mais nada abaixo
+# Exceto por sua conta e risco
+
+# Captura data do envio do backup
+DATA=$(date +%d/%m/%Y)
+
+# Mensagens para email e notificação do Telegram
+MSG="Mikrotik - Backup do dia $DATA"
+
+# Conecta-se ao Mikrotik via SSH
 sshpass -p $MK_PASS ssh $MK_USER@$MK_IP "echo conectado via SSH no Mikrotik"
+# Verifica se a conexão SSH foi bem sucedida 
 if [ $? -ne 0 ]; then
+  # Exibe mensagem de erro de conexão ao Mikrotik via SSH
   echo "Erro: não foi possível se conectar ao Mikrotik via SSH"
   exit 1
 fi
 
-# Envia e-mail pelo Mikrotik
- sshpass -p $MK_PASS ssh $MK_USER@$MK_IP '/tool e-mail send to="'"$EMAIL_TO"'" subject="'"$EMAIL_SUBJECT"'" body="'"$EMAIL_BODY"'" file='"$MK_BCKP_FILE"
-echo "Email enviado pelo Mikrotik"
+# Envia o backup por e-mail pelo Mikrotik
+sshpass -p $MK_PASS ssh $MK_USER@$MK_IP '/tool e-mail send to="'"$EMAIL"'" subject="'"$MSG"'" body="'"$MSG"'" file='"$MK_BCKP_FILE"
+# Exibe mensagem de envio do backup por email pelo Mikrotik
+echo "Backup enviado por email pelo Mikrotik"
 
-# Faz o download do arquivo
+# Faz o download do arquivo de dentro do Mikrotik
 sshpass -p $MK_PASS scp $MK_USER@$MK_IP:/$MK_BCKP_FILE .
+# Verifica se o download foi bem sucedido
 if [ $? -ne 0 ]; then
+  # Exibe mensagem de erro ao fazer download do arquivo de backup
   echo "Erro ao fazer download do arquivo de backup"
   exit 1
 fi
 
-# Verifica se o arquivo de backup foi realmente baixado
+# Verifica se o arquivo de backup foi realmente baixado do Mikrotik
 if [ ! -f $MK_BCKP_FILE ]; then
+  # Exibe mensagem de erro informando que o backup não foi baixado 
   echo "Erro: arquivo de backup não foi baixado"
   exit 1
 fi
 
-# Exibe uma mensagem de sucesso
+# Exibe uma mensagem de que o arquivo de backup foi baixado com sucesso
 echo "Arquivo de backup baixado com sucesso!"
 
-# Compacta o arquivo de backup
+# Compacta o arquivo de backup e armazena na variável ARQ_BKP
 if [ "$COMPAC" = "zip" ]; then
   zip -r "$MK_BCKP_FILE.zip" "$MK_BCKP_FILE"
   ARQ_BKP="$MK_BCKP_FILE.zip"
@@ -57,8 +70,9 @@ else
   tar -zcvf "$MK_BCKP_FILE.tar.gz" "$MK_BCKP_FILE"
   ARQ_BKP="$MK_BCKP_FILE.tar.gz"
 fi
+# Exibe uma mensagem de que o arquivo foi compactado
 echo "Arquivo compactado"
 
-# Envia o backup baixado para o telegram
-curl -F document=@"$ARQ_BKP" -F caption="EMAIL_SUBJECT" "https://api.telegram.org/bot${TOKEN}/sendDocument?chat_id=$CHATID" &>/dev/null
+# Envia o backup baixado para o Telegram
+curl -F document=@"$ARQ_BKP" -F caption="$MSG" "https://api.telegram.org/bot${TOKEN}/sendDocument?chat_id=$CHATID" &>/dev/null
 echo "Arquivo enviado para o Telegram"
